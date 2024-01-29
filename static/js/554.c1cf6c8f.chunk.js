49eb29aab56d25417db411283d44a4c54aac381c
inhasid <137834139+inhasid@users.noreply.github.com>
2024-01-29 11:34:23 +0000

    Deploying to gh-pages from @ inhasid/goit-react-hw-05-movies@7815d77534538f718f6ac8d59c688ee676d01e4c 🚀

---
+++ static/js/554.c1cf6c8f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[554],{554:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(87),a="not-found-page_title__S4jvl",o=n(184),c=function(){return(0,o.jsxs)("div",{className:a,children:[(0,o.jsx)("h1",{children:"Cannot found this page"}),(0,o.jsx)(r.rU,{to:"/",children:"To home page"})]})}}}]);
+//# sourceMappingURL=554.c1cf6c8f.chunk.js.map
