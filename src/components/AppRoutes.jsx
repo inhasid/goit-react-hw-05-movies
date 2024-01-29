d65b9e70c98659821051160cb2c9c2c61f242e1e
inhasid <sidorchuk.inga@gmail.com>
2024-01-29 12:31:52 +0100

    lazy

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
 
 import SharedLayout from './SharedLayout/SharedLayout';
 
-import HomePage from 'pages/HomePage/HomePage';
-import MoviesPage from 'pages/MoviesPage/MoviesPage';
-import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
-import MovieDetailsPage from 'pages/MovieDetailsPage/MovieDetailsPage';
-import Cast from 'components/Cast/Cast';
-import Reviews from 'components/Reviews/Reviews';
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('pages/MoviesPage/MoviesPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
+const MovieDetailsPage = lazy(() =>
+  import('pages/MovieDetailsPage/MovieDetailsPage')
+);
+const Cast = lazy(() => import('components/Cast/Cast'));
+('components/Cast/Cast');
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 const AppRoutes = () => {
   return (
